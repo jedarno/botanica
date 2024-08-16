320c76f3fe3947e843a78dfb28b0d2bbdb1d039b
Jed Arno <jedarno99@gmail.com>
2024-08-16 19:32:49 +0100

    Hybrid and concat features

--- transformer/architcture.py
+++ transformer/architcture.py
@@ -87,6 +87,7 @@ class transformer_ensemble_weighted(nn.Module):
 
         if not np.issubdtype(weights.dtype, np.number):
             raise ValueError("weights should be numeric")
+
         print("New model influence weights: {}".format(weights))
         self._weights = weights
 
@@ -99,5 +100,88 @@ class transformer_ensemble_weighted(nn.Module):
             output = output + self.weights[i] * self.sftmx(base_out)
 
         return output
+
+class hybrid_features_ViTb(nn.Module):
+
+    def __init__(self, models, n_classes, weights=None):
+        """
+        Model heads should be removed before being added to hyrbrid_features model
+        """
+        super(hybrid_features_ViTb, self).__init__()
+        self.models = models
+
+        if weights:
+            self.weights = weights
+
+        #Remove classification heads
+        for model in self.models:
+            model.heads.head = nn.Identity()
+
+        #Define new classifier
+        self.classifier = nn.Sequential(
+            nn.Linear(768, 512),
+            nn.ReLU(),
+            nn.Dropout(p=0.3, inplace=False),
+            nn.Linear(512, n_classes)
+            )
+
+    @property
+    def weights(self):
+        return self._weights
+
+    @weights.setter
+    def weights(self, weights):
+
+        if type(weights) == list:
+          weights = np.array(weights)
+
+        if len(weights) != len(self.models):
+            raise ValueError("The number of  weight values provided should mathch the number of models.")
+
+        elif not np.issubdtype(weights.dtype, np.number):
+            raise ValueError("weights should be numeric")
+
+        else:
+          print("New model influence weights: {}".format(weights))
+          self._weights = weights
+
+    def forward(self, x):
+        out = self.weights[0] * self.models[0](x)
+
+        for i in range (1,len(self.models)):
+            out += self.models[i](x) * self.weights[i]
+
+        out = out/len(self.models)
+        out = self.classifier(out)
+
+        return out
+
+
+class feature_concat_ViT_SWIN(nn.Module):
+
+  def __init__(self, vit_model, swin_model):
+    super(feature_concat_morelayers, self).__init__()
+    self.vit_model = vit_model
+    self.swin_model = swin_model
+
+    #remove class heads
+    self.vit_model.heads.head = nn.Identity()
+    self.swin_model.head = nn.Identity()
+
+    #define new classifier
+    self.classifier = nn.Sequential(
+      nn.Linear(768 + 1024, 1024),
+      nn.ReLU(),
+      nn.Dropout(p=0.3, inplace=False),
+      nn.Linear(1024, 101)
+     )
+  def forward(self, x):
+    vit_features = self.vit_model(x.clone())
+    vit_features = vit_features.view(vit_features.size(0), -1)
+    swin_features = self.swin_model(x)
+    swin_features = swin_features.view(swin_features.size(0), -1)
+    x = torch.cat((vit_features, swin_features), dim=1)
+    x = self.classifier(x)
+    return x
 
 
