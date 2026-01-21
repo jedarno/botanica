1aaacbf7705255b2521f5a79dd0800d3ce59dc40
Jed Arno <zfac230@CIM-STF-00777.local>
2026-01-21 16:21:31 +0000

    swin scheduler change

--- transformer/utils.py
+++ transformer/utils.py
@@ -347,7 +347,7 @@ def train_model_wrapper_swin_b(params, trainloader, trainset, valloader, valset,
   criterion = LabelSmoothingCrossEntropy()
   criterion = criterion.to(device)
   optimizer = optim.AdamW(tune_params, lr=params[0], betas = (params[1], 0.999), weight_decay=params[2])
-  exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+  exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
 
   model = get_train_model(model, criterion, optimizer, exp_lr_scheduler, trainloader, trainset, valloader, valset, device, num_epochs=num_epochs)
 
