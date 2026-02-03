f7eafaf009904fd06fd6221b5874c7344ff3903f
Jed Arno <zfac230@CIM-STF-00777.local>
2026-02-03 18:29:10 +0000

    parameters

--- transformer/utils.py
+++ transformer/utils.py
@@ -214,7 +214,7 @@ def train_model_wrapper_vit_b(params, trainloader, trainset, valloader, valset, 
 
   criterion = LabelSmoothingCrossEntropy()
   criterion = criterion.to(device)
-  optimizer = optim.AdamW(model.heads.head.params, lr=params[0], betas = (params[1], 0.999), weight_decay=params[2])
+  optimizer = optim.AdamW(model.heads.head.paramaters(), lr=params[0], betas = (params[1], 0.999), weight_decay=params[2])
   exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
 
   model = get_train_model(model, criterion, optimizer, exp_lr_scheduler, trainloader, trainset, valloader, valset, device, num_epochs=num_epochs)
