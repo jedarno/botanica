f08e7d808ccb7c8ae8989d6239a5ed894f95b0de
Jed Arno <zfac230@CIM-STF-00777.local>
2026-03-19 16:58:23 +0000

    swarm fitness func for vitb

--- transformer/utils.py
+++ transformer/utils.py
@@ -210,11 +210,19 @@ def train_model_wrapper_vit_b(params, trainloader, trainset, valloader, valset, 
   for param in model.heads.head.parameters():
     param.requires_grad = True
 
+  for param in model.encoder.layers.encoder_layer_11.parameters():
+    param.requries_grad = True
+
+  for param in model.encoder.ln.parameters():
+    param.requires_grad = True
+
+  tune_params = [{'params' : model.encoder.layers.encoder_layer_11.parameters()}, {'params' : model.encoder.ln.parameters()}, {'params' : model.heads.head.parameters()}]
+
   model = model.to(device)
 
   criterion = LabelSmoothingCrossEntropy()
   criterion = criterion.to(device)
-  optimizer = optim.AdamW(model.heads.head.parameters(), lr=params[0], betas = (params[1], 0.999), weight_decay=params[2])
+  optimizer = optim.AdamW(tune_params, lr=params[0], betas = (params[1], 0.999), weight_decay=params[2])
   exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
 
   loss = train_model_loss(model, criterion, optimizer, exp_lr_scheduler, trainloader, trainset, valloader, valset, device, num_epochs=num_epochs)
